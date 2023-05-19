ffaa2dbce1df6c2e5e09ebad0a09ac16726b28a2
Bauke Molenaar <bauke.molenaar@gmail.com>
2023-05-19 03:48:46 +0200

    modified:   .config/config 	modified:   Scripts/Local/menu.sh

--- Scripts/Local/menu.sh
+++ Scripts/Local/menu.sh
@@ -29,10 +29,10 @@ fi
 if [ "$option" == "Flash Sd" ]; then
     cd "${DOWNLOAD_DIR}" || exit
     if [ ! -f "${SD_DEVICE}" ]; then
-        echo "${SD_DEVICE} is not found, make sure the corect value is set in .config/config..."
+        echo "${SD_DEVICE} value is not set, make sure the corect value is set in .config/config..."
         exit
     else
-        echo "SD_DEVICE is set to ${SD_DEVICE}"
+        echo "SD_DEVICE value read from .config/config is ${SD_DEVICE}"
         if [ ! -f pwnagotchi-raspbian-lite-${PWNAGOTCHI_VERSION}.zip ]; then
             echo "${DOWNLOAD_DIR}/${PWNAGOTCHI_VERSION} is not found, download it first and try again..."
             exit
