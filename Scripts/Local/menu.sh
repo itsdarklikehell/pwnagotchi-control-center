a04c0813354923016f2030183785aad26fa37029
Bauke Molenaar <bauke.molenaar@gmail.com>
2023-05-19 02:18:41 +0200

    modified:   Scripts/Local/menu.sh 	modified:   .config/config

--- Scripts/Local/menu.sh
+++ Scripts/Local/menu.sh
@@ -11,9 +11,14 @@ option=$(
 if [ "$option" == "Download Image" ]; then
     cd "${DOWNLOAD_DIR}" || exit
     wget https://github.com/evilsocket/pwnagotchi/releases/download/"${PWNAGOTCHI_VERSION}"/pwnagotchi-raspbian-lite-"${PWNAGOTCHI_VERSION}".zip
+    unzip -p pwnagotchi-raspbian-lite-"${PWNAGOTCHI_VERSION}".zip
 fi
 
 if [ "$option" == "Flash Sd" ]; then
     cd "${DOWNLOAD_DIR}" || exit
-    unzip -p pwnagotchi-raspbian-lite-"${PWNAGOTCHI_VERSION}".zip #| dd of=/dev/sdb bs=1M
+    if [ -f pwnagotchi-raspbian-lite-"${PWNAGOTCHI_VERSION}".zip ]; then
+        unzip -p pwnagotchi-raspbian-lite-"${PWNAGOTCHI_VERSION}".zip | dd of="${SD_DEVICE}" bs=1M
+    else
+        echo "${DOWNLOAD_DIR}/${PWNAGOTCHI_VERSION} is not found, download it first and try again..."
+    fi
 fi
