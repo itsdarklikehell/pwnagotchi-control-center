5d8f099aa34dffc49c40902bf4020e99bf605368
Bauke Molenaar <bauke.molenaar@gmail.com>
2023-05-20 19:53:20 +0200

    modified:   Scripts/Local/pull-files.sh 	modified:   Scripts/Local/push-files.sh

--- Scripts/Local/pull-files.sh
+++ Scripts/Local/pull-files.sh
@@ -5,7 +5,7 @@ MOUNT() {
         echo "$SD_DEVICE Variable is not set, make sure the corect value is set in .config/config..."
         exit
     else
-        if (whiptail --title "Are you sure you want to mount $SD_DEVICE?" --yesno "Unmount check." $LINES $COLUMNS); then
+        if (whiptail --title "Are you sure you want to mount $SD_DEVICE?" --yesno "Mount check." $LINES $COLUMNS); then
             echo "User selected Yes, exit status was $?."
             sudo mount -a
         else
@@ -21,7 +21,7 @@ BACKUP_DIR=$(
 )
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered $BACKUP_DIR"
+    echo "User selected Ok and entered $BACKUP_DIR."
 else
     echo "User selected Cancel."
     exit
@@ -37,7 +37,7 @@ SD_DEVICE=$(
 )
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered $SD_DEVICE"
+    echo "User selected Ok and entered $SD_DEVICE."
 else
     echo "User selected Cancel."
     exit
@@ -47,14 +47,15 @@ FILES=$(
     whiptail --title "Check list example" --checklist \
         "Choose what you want to Pull" $LINES $COLUMNS $(($LINES - 8)) \
         "Handshakes" "Pull handshakes from $ROOT_MOUNT_DIR/$HANDSHAKE_DIR to $BACKUP_DIR/Handshakes" ON \
-        "Main Config" "Pull main config.toml from /etc/pwnagotchi/config.toml to $BACKUP_DIR/Configs" ON \
+        "Main Config" "Pull main config.toml from $ROOT_MOUNT_DIR/etc/pwnagotchi/config.toml to $BACKUP_DIR/Configs" ON \
         "Plugin Files" "Pull plugin files from $CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Plugins" ON \
         "Plugin Configs" "Pull plugin configs from $CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Configs" ON \
+        "User Bin" "Pull user bin from $ROOT_MOUNT_DIR/home/pi/bin to $BACKUP_DIR/Bin" ON \
         3>&1 1>&2 2>&3
 )
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered $FILES"
+    echo "User selected Ok and entered $FILES."
 else
     echo "User selected Cancel."
     exit
@@ -64,7 +65,7 @@ if [[ "$FILES" =~ "Handshakes" ]]; then
     if [ ! -d "$BACKUP_DIR/Handshakes" ]; then
         mkdir -P "$BACKUP_DIR/Handshakes"
     fi
-    echo "Pulling handshakes from $ROOT_MOUNT_DIR/$HANDSHAKE_DIR to $BACKUP_DIR/Handshakes"
+    echo "Pulling handshakes from $ROOT_MOUNT_DIR/$HANDSHAKE_DIR to $BACKUP_DIR/Handshakes."
     cp "$ROOT_MOUNT_DIR/$HANDSHAKE_DIR/*" "$BACKUP_DIR/Handshakes"
 fi
 
@@ -72,7 +73,7 @@ if [[ "$FILES" =~ "Main Config" ]]; then
     if [ ! -d "$BACKUP_DIR/Configs" ]; then
         mkdir -P "$BACKUP_DIR/Configs"
     fi
-    echo "Pulling main config.toml from /etc/pwnagotchi/config.toml to $BACKUP_DIR/Configs/config.toml and /boot/config.toml"
+    echo "Pulling main config.toml from $ROOT_MOUNT_DIR/etc/pwnagotchi/config.toml to $BACKUP_DIR/Configs/config.toml and /boot/config.toml."
     cp "$ROOT_MOUNT_DIR/etc/pwnagotchi/config.toml" "$BACKUP_DIR/Configs/config.toml"
     cp "$BOOT_MOUNT_DIR/config.toml" "$BACKUP_DIR/Configs/config.toml"
 fi
@@ -81,7 +82,7 @@ if [[ "$FILES" =~ "Plugin Files" ]]; then
     if [ ! -d "$BACKUP_DIR/Plugins" ]; then
         mkdir -P "$BACKUP_DIR/Plugins"
     fi
-    echo "Pulling plugin files from $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Plugins"
+    echo "Pulling plugin files from $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Plugins."
     cp "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR/*" "$BACKUP_DIR/Plugins"
 fi
 
@@ -89,8 +90,13 @@ if [[ "$FILES" =~ "Plugin Configs" ]]; then
     if [ ! -d "$BACKUP_DIR/Plugins" ]; then
         mkdir -P "$BACKUP_DIR/Plugins"
     fi
-    echo "Pulling plugin configs from $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Plugins"
+    echo "Pulling plugin configs from $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Plugins."
     cp "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR/*.toml" "$BACKUP_DIR/Plugins"
     cp "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR/*.yml" "$BACKUP_DIR/Plugins"
     cp "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR/*.yaml" "$BACKUP_DIR/Plugins"
+fi
+
+if [[ "$FILES" =~ "User Bin" ]]; then
+    echo "Pulling files from $ROOT_MOUNT_DIR/home/pi/bin to $BACKUP_DIR/Bin."
+    cp "$ROOT_MOUNT_DIR/home/pi/bin/*" "$BACKUP_DIR/Bin"
 fi
--- Scripts/Local/push-files.sh
+++ Scripts/Local/push-files.sh
@@ -5,7 +5,7 @@ MOUNT() {
         echo "$SD_DEVICE Variable is not set, make sure the corect value is set in .config/config..."
         exit
     else
-        if (whiptail --title "Are you sure you want to mount $SD_DEVICE?" --yesno "Unmount check." $LINES $COLUMNS); then
+        if (whiptail --title "Are you sure you want to mount $SD_DEVICE?" --yesno "Mount check." $LINES $COLUMNS); then
             echo "User selected Yes, exit status was $?."
             sudo mount -a
         else
@@ -21,7 +21,7 @@ BACKUP_DIR=$(
 )
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered $BACKUP_DIR"
+    echo "User selected Ok and entered $BACKUP_DIR."
 else
     echo "User selected Cancel."
     exit
@@ -37,7 +37,7 @@ SD_DEVICE=$(
 )
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered $SD_DEVICE"
+    echo "User selected Ok and entered $SD_DEVICE."
 else
     echo "User selected Cancel."
     exit
@@ -46,39 +46,45 @@ fi
 FILES=$(
     whiptail --title "Check list example" --checklist \
         "Choose what you want to Push" $LINES $COLUMNS $(($LINES - 8)) \
-        "Handshakes" "Push handshakes from $ROOT_MOUNT_DIR/$HANDSHAKE_DIR to $BACKUP_DIR/Handshakes" ON \
-        "Main Config" "Push main config.toml from /etc/pwnagotchi/config.toml to $BACKUP_DIR/Configs" ON \
-        "Plugin Files" "Push plugin files from $CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Plugins" ON \
-        "Plugin Configs" "Push plugin configs from $CUSTOM_PLUGIN_DIR to $BACKUP_DIR/Configs" ON \
+        "Handshakes" "Push handshakes from $BACKUP_DIR/Handshakes to $ROOT_MOUNT_DIR/$HANDSHAKE_DIR" ON \
+        "Main Config" "Push main config.toml from $BACKUP_DIR/Configs to $ROOT_MOUNT_DIR/etc/pwnagotchi/config.toml" ON \
+        "Plugin Files" "Push plugin files from $BACKUP_DIR/Plugins to $CUSTOM_PLUGIN_DIR" ON \
+        "Plugin Configs" "Push plugin configs from $BACKUP_DIR/Configs to $CUSTOM_PLUGIN_DIR" ON \
+        "User Bin" "Push user bin from $BACKUP_DIR/Bin to /home/pi/bin" ON \
         3>&1 1>&2 2>&3
 )
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    echo "User selected Ok and entered $FILES"
+    echo "User selected Ok and entered $FILES."
 else
     echo "User selected Cancel."
     exit
 fi
 
 if [[ "$FILES" =~ "Handshakes" ]]; then
-    echo "Pushing handshakes from $BACKUP_DIR/Handshakes to $ROOT_MOUNT_DIR/$HANDSHAKE_DIR"
+    echo "Pushing handshakes from $BACKUP_DIR/Handshakes to $ROOT_MOUNT_DIR/$HANDSHAKE_DIR."
     cp "$BACKUP_DIR/Handshakes/*" "$ROOT_MOUNT_DIR/$HANDSHAKE_DIR"
 fi
 
 if [[ "$FILES" =~ "Main Config" ]]; then
-    echo "Pushing main config.toml from $BACKUP_DIR/Configs/config.toml to /etc/pwnagotchi/config.toml and /boot/config.toml"
+    echo "Pushing main config.toml from $BACKUP_DIR/Configs/config.toml to $ROOT_MOUNT_DIR/etc/pwnagotchi/config.toml and $BOOT_MOUNT_DIR/boot/config.toml."
     cp "$BACKUP_DIR/Configs/config.toml" "$ROOT_MOUNT_DIR/etc/pwnagotchi/config.toml"
     cp "$BACKUP_DIR/Configs/config.toml" "$BOOT_MOUNT_DIR/config.toml"
 fi
 
 if [[ "$FILES" =~ "Plugin Files" ]]; then
-    echo "Pushing plugin files from $BACKUP_DIR/Plugins to $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR"
+    echo "Pushing plugin files from $BACKUP_DIR/Plugins to $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR."
     cp "$BACKUP_DIR/Plugins/*" "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR"
 fi
 
 if [[ "$FILES" =~ "Plugin Configs" ]]; then
-    echo "Pushing plugin configs from $BACKUP_DIR/Plugins to $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR"
+    echo "Pushing plugin configs from $BACKUP_DIR/Plugins to $ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR."
     cp "$BACKUP_DIR/Plugins" "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR/*.toml"
     cp "$BACKUP_DIR/Plugins" "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR/*.yml"
     cp "$BACKUP_DIR/Plugins" "$ROOT_MOUNT_DIR/$CUSTOM_PLUGIN_DIR/*.yaml"
+fi
+
+if [[ "$FILES" =~ "User Bin" ]]; then
+    echo "Pushing files from $BACKUP_DIR/Bin to $ROOT_MOUNT_DIR/home/pi/bin."
+    cp "$BACKUP_DIR/Bin/*" "$ROOT_MOUNT_DIR/home/pi/bin"
 fi
