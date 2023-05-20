9a0742a5d722bc1ef30588c59c7379201fb4db86
Bauke Molenaar <bauke.molenaar@gmail.com>
2023-05-20 02:24:04 +0200

    modified:   menu.sh

--- menu.sh
+++ menu.sh
@@ -11,6 +11,7 @@ option=$(
     whiptail --title "Main Menu." --menu "Choose an option" $LINES $COLUMNS $(($LINES - 8)) \
         "Local Stuff" "Scripts to run locally (ie. Flash or backup an sd card or edit and apply a config.toml to or from a mounted card)." \
         "Remote Stuff" "Setup connection and run scripts on a remotely running pwnagotchi via ssh over usb/eth/bt-pan/wifi(ap)." \
+        "Update" "update this script." \
         3>&1 1>&2 2>&3
 )
 exitstatus=$?
@@ -27,4 +28,8 @@ fi
 
 if [ "$option" == "Remote Stuff" ]; then
     ./Scripts/Remote/menu.sh
+fi
+
+if [ "$option" == "Update" ]; then
+    git pull origin master
 fi
